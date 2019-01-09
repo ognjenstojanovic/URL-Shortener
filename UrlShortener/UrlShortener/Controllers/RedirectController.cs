5e1173d0847a90b43bb16282a931be11bd19b294
Ognjen Stojanovic <ognjenstojanovic55@gmail.com>
2019-01-09 02:18:13 +0100

    Initial commit, gitignore added. Shorten and Redirect controllers with methods added.

---
+++ UrlShortener/UrlShortener/Controllers/RedirectController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortener.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class RedirectController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            // retreive record from database
+            return Redirect("http://www.google.com");
+        }
+    }
+}
---
+++ UrlShortener/UrlShortener/Controllers/ShortenController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortener.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShortenController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            // Insert record to database
+            return Ok();
+        }
+    }
+}
